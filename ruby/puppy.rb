eea7e3d0dcc6778e40c0124e947e04e24c7c93c7
bw2116 <bw2116@gmail.com>
2016-11-29 19:40:58 -0600

    puppy class design

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+kingdom ------------------------
+animal
+
+characteristics ----------------
+fur color: varies
+makes noise: yes
+nose count: 1
+eye count: 2
+ear count: 2
+
+behavior -----------------------
+barks
+wags tail
+eats
+pops
+sleeps
