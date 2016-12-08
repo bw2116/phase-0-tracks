c920e52597043cb0f146a968c3350cd62e69a357
bw2116 <bw2116@gmail.com>
2016-12-07 18:10:14 -0600

    explore.js file complete

---
+++ js/explore.js
@@ -0,0 +1,14 @@
+// 1. read in text 
+// 2. split text into individual letters
+// 3. reverse the letters
+
+// var str = "test";
+
+
+function reverse(str) {
+  if("yes" == "yes")
+  var newStr = str.split("").reverse().join("");
+  return newStr
+}
+
+console.log(reverse("test"));
