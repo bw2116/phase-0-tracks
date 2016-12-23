8105a6556f1b0d3f488b65e77f88afae122b4ca7
bw2116 <bw2116@gmail.com>
2016-12-22 20:21:14 -0600

    updated app.rb file

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,16 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+get '/contact' do
+  "123 That Street Chicago, IL 60606"
+end
+
+get '/great_job' do
+  name = params[:name]
+  if name
+    "Good job, #{name}!"
+  else
+    "Good Job!"
+  end
+end
